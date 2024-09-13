91f92377fff070fefecc386058cdd0e06c942b47
TIXAR-PANDA <natali040168@mail.ru>
2024-09-13 17:03:27 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,11 +3,17 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 # print ("Hello, world!" )
+# 1. В переменную example запишите любую строку.
 name="Жгутиковые"
 print(name)
+# 2. Выведите на экран (в консоль) первый символ этой строки.
 print(name[0])
+# 3. Выведите на экран(в консоль) последний символ этой строки (используя отрицательный индекс).
 print(name[-1])
-print(name[4:])
+# 4.Выведите на экран(в консоль) вторую половину этой строки (С нечётным количеством символов: 'Urban' -> 'ban').
+print(name[5:])
+# 5. Выведите на экран(в консоль) это слово наоборот.
 print(name[::-1])
-print(name[0::2])
+# 6. Выведите на экран(в консоль) каждый второй символ этой строки.
+print(name[1::2])
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
