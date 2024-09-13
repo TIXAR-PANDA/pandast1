74d0dcde86da8d2803576eb4ed4e2f5145fcfe94
TIXAR-PANDA <natali040168@mail.ru>
2024-09-13 08:00:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# print ("Hello, world!" )
+name="Жгутиковые"
+print(name)
+print(name[0])
+print(name[-1])
+print(name[4:])
+print(name[::-1])
+print(name[0::2])
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
