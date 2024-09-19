513f0101a6acb84f85f2d81d25190f796a98033b
TIXAR-PANDA <natali040168@mail.ru>
2024-09-19 11:22:48 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,13 @@
+# Условная конструкция. Операторы if, elif, else."
+
+first=int( input("первое число:"))
+second=int( input("второе число:"))
+third=int( input("третье число:"))
+if first==second and second==third :
+    print (3)
+elif first==second or second==third or first==third:
+    print(2)
+#elif first != second and second != third and first != third:
+    #print(0)
+else:
+    print (0)
